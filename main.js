c311a2c639d327a35f8a8077deb4e96a312b04c1
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2020-06-17 14:42:05 -0400

    app works

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 // Your code here!
 
+const colorCombinator = require('./color-combinator.js');
 
+const colorDeconstructor = require('./color-deconstructor.js');
+
+const colorOne = getInput(0);
+
+const colorTwo = getInput(1);
+
+
+
+if (colorOne === undefined) {
+  console.log("Enter a color or get smacked.");
+} else if (colorTwo === undefined) {
+  console.log(colorDeconstructor(colorOne));
+} else {
+  console.log(colorCombinator(colorOne, colorTwo));
+}
 
 // Our helper function here! Don't  touch!
 function getInput(i) {
